76e8c8febd61d9f342bf1ca1f6e90ddc130c8f76
aul.git <fahri.aulady101@gmail.com>
2021-01-20 08:21:58 +0700

    TUGAS HARI 7

--- TugasPythonLanjutan_Hari_7_AuladiFachryAssidiqi.py
+++ TugasPythonLanjutan_Hari_7_AuladiFachryAssidiqi.py
@@ -1,5 +1,16 @@
 import pandas as pd
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
+def kompas():
+    KOMPAS = "https://www.kompas.com/"
+    html = urlopen(KOMPAS)
+    data = BeautifulSoup(html, 'html.parser')
 
+    items = data.findAll("h4", {"class":"most__title"})
+    data = []
+    for item in items:
+        data.append(item.get_text())
+    print(pd.DataFrame(data, columns=['Judul Berita Terpopuler']))
+
+kompas()
